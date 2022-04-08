f5efb053cee3f92f6a75957eceeb85d7a1efa5cc
CorneaRaduValentin <radu-valentin.cornea@student.tuiasi.ro>
2022-04-08 14:56:07 +0300

    Changed to Single Page Application

--- continut/js/script.js
+++ continut/js/script.js
@@ -72,13 +72,13 @@ function drawCanvas(){
 
 function schimbaContinut(resursa){
     var xhttp = new XMLHttpRequest();
+
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-       document.getElementById("demo").innerHTML = this.responseText;
+       document.getElementById("continut").innerHTML = this.responseText;
       }
     };
     xhttp.open("GET", resursa + '.html', true);
     xhttp.send();
 
-    document.getElementById("continut").innerHTML = resursa
 }
